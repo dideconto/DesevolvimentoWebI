697678a5a752509a679760c2a1904a23391acc59
Diogo Deconto <dideconto@gmail.com>
2020-05-27 21:13:12 -0300

    Javascript e Bootstrap

--- 2Bimestre/scripts/domb.js
+++ 2Bimestre/scripts/domb.js
@@ -1,8 +1,39 @@
 var txtNumero1 = document.getElementById('txtNumero1');
 var txtNumero2 = document.querySelector('#txtNumero2');
 var btnSomar = document.querySelector('#btnSomar');
+var btnLimpar = document.querySelector('#btnLimpar');
+var btnAdicionar = document.querySelector('#btnAdicionar');
+var txtResultado = document.querySelector('#txtResultado');
+var lblResultado = document.querySelector('#lblResultado');
+var lstTeste = document.querySelector('#lstTeste');
+
+var contador = 0;
 
 btnSomar.onclick = function(){
     //O CÓDIGO DENTRO DESTA FUNÇÃO SERÁ EXECUTADO AO CLICAR NO BOTÃO
-    alert('Clicamos no botão');
+    var soma = parseInt(txtNumero1.value) + parseInt(txtNumero2.value);
+
+    txtResultado.value = 'Resultado: ' + soma;
+    
+    var textoResultado = document.createTextNode('Resultado: ' + soma);
+    lblResultado.appendChild(textoResultado);
+
+    // alert('Soma: ' + soma);
+    // console.log('Soma: ' + soma);
+};
+
+btnLimpar.onclick = function(){
+    lblResultado.innerHTML = '';
+    txtResultado.value = '';
+    lstTeste.innerHTML = '';
+};
+
+btnAdicionar.onclick = function(){
+    var elementoItem = document.createElement('li');
+    var textoItem = document.createTextNode(contador);
+    elementoItem.appendChild(textoItem);
+
+    lstTeste.appendChild(elementoItem);
+
+    contador++;
 };
---
+++ 2Bimestre/scripts/todoa.js
@@ -0,0 +1,8 @@
+//Pegar a informação da caixa de texto
+//Criar um elemento para a linha
+//Criar os elementos das células 
+//Adicionar o conteúdo textual
+//Adicionar a linha dentro da tabela
+
+
+var element = document.createElement('tr');
